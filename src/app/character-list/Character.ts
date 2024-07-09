281bb842621ad1034825b9bf292ec14932bc8857
Carolina Larralde <carogise9@gmail.com>
2024-07-09 12:29:41 -0300

    Clase 3 modificaciones

---
+++ src/app/character-list/Character.ts
@@ -0,0 +1,11 @@
+export interface Character{
+    retrato:string;
+    nombre:string;
+    edad:number;
+    faccion:string;
+    estatus:string;
+    rol:string;
+    disponible:boolean;
+    principal:boolean;
+
+}
--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from './character';
 
 @Component({
   selector: 'app-character-list',
@@ -6,42 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './character-list.component.scss',
 })
 export class CharacterListComponent implements OnInit {
-  personaje = {
-    retrato: "img/v.jpg",
-    nombre: 'Valerie/Vincent (V)',
-    edad: 24,
-    faccion: 'Merc',
-    estatus:
-      'Fallecido (Asesinado por tener implantado el engrama de Johnny Silverhand)',
-    rol: 'Personaje principal',
-  };
-  
-  personajeDos = {
-    retrato: "img/Johnny.jpg",
-    nombre: 'Johnny Silverhand',
-    edad: '34 (Fisico) - 88 (Engrama)',
-    faccion: 'Samurai',
-    estatus: 'Fallecido (Asesinado por Adam Smasher)',
-    rol: 'Personaje principal',
-  };
+  personajes: Character[] = [
+    {
+      retrato: 'img/v.jpg',
+      nombre: 'Valerie/Vincent (V)',
+      edad: 24,
+      faccion: 'Merc',
+      estatus:
+        'Fallecido (Asesinado por tener implantado el engrama de Johnny Silverhand)',
+      rol: 'Personaje principal',
+      disponible:true,
+      principal:true,
+    },
+    {
+      retrato: 'img/Johnny.jpg',
+      nombre: 'Johnny Silverhand',
+      edad: 88,
+      faccion: 'Samurai',
+      estatus: 'Fallecido (Asesinado por Adam Smasher)',
+      rol: 'Personaje principal',
+      disponible:true,
+      principal:true,
+    },
+    {
+      retrato: 'img/songbird.jpg',
+      nombre: 'Song So Mi (Songbird)',
+      edad: 31,
+      faccion: 'FIA',
+      estatus:
+        'Desconocido (1.Asesinada por V o por la FIA. 2.Enviada a la Luna)',
+      rol: 'Personaje principal',
+      disponible:true,
+      principal:true,
+    },
 
-  personajeTres = {
-    retrato: "img/songbird.jpg",
-    nombre: 'Song So Mi (Songbird)',
-    edad: 31,
-    faccion: 'FIA',
-    estatus:
-      'Desconocido (1.Asesinada por V o por la FIA. 2.Enviada a la Luna)',
-    rol: 'Personaje principal',
-  };
-  personajeCuatro = {
-    retrato: "img/evelyn.jpg",
-    nombre: 'Evelyn Parker',
-    edad: 'Desconocida',
-    faccion: 'MOX',
-    estatus: 'Fallecida (Suicidio)',
-    rol: 'Personaje Secundario',
-  };
+    {
+      retrato: 'img/evelyn.jpg',
+      nombre: 'Evelyn Parker',
+      edad: 34,
+      faccion: 'MOX',
+      estatus: 'Fallecida (Suicidio)',
+      rol: 'Personaje Secundario',
+      disponible:true,
+      principal:false,
+    },
+  ];
+
   constructor() {}
   ngOnInit(): void {}
 }
